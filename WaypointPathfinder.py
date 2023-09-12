e7eacd31ea1324ce64745e9438bce3b528f11a78
ioliva0 <90651651+ioliva0@users.noreply.github.com>
2023-09-12 09:33:03 -0400

    added documentation

--- WaypointPathfinder.py
+++ WaypointPathfinder.py
@@ -1,12 +1,19 @@
+"""
+This code takes in waypoints in the format (x, y, z) (all in centimeters)
+and computes the shortest path between them
+"""
+
+
+
 #this code can be optimized; let's look here if we have performance issues
 
 from math import sqrt
 
+#Creates a table of the distance between every pair of 2 waypoints
+#This way we don't have to compute these over and over, speeding up the algorithm
 def find_distances(waypoints):
 
     waypoint_distances = {}
-
-    #TODO: FIX THE LOGIC SO IT ISNT TRASH
 
     numpoints = len(waypoints)
 
@@ -24,13 +31,14 @@ def find_distances(waypoints):
     
     return waypoint_distances
 
+#This is the 
 def find_path(waypoints):
     waypoint_distances = find_distances(waypoints)
     print(waypoint_distances)
 
 
 
-#run a test if WaypointOptimizer is run directly
+#run a test if the Pathfinder script is run directly
 #(as opposed to being imported into a different script)
 if __name__ == "__main__":
     find_path([(5, 2, 1), (0,0,0), (2,3,2), (1,1,1)])
