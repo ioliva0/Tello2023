7be4a4a3e98452ecb2961ad256a1a90b65e89cad
ioliva0 <90651651+ioliva0@users.noreply.github.com>
2023-10-23 08:58:07 -0400

    ho

--- src/DrawNumbers.py
+++ src/DrawNumbers.py
@@ -1,5 +1,5 @@
 from djitellopy import tello, Tello
-
+import cv2
 from Utils.Hover import hover
 
 tello = Tello()
@@ -49,8 +49,40 @@ def draw_num(tagNum):
             tello.curve_xyz_speed(-50, 50, 0, -100, 0, 0, 50)
             tello.curve_xyz_speed(50, -50, 0, 100, 0, 0, 50)
             tello.curve_xyz_speed(-100, -100, 0, -200, 0, 0, 50)
-
-    tello.land()
     #hover(tello, 6)
 
-draw_num(9)
+# Create a window for the video stream
+window = "Tello Stream"
+cv2.namedWindow(window)
+
+# Define the ArUco dictionary
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+arucoParams = cv2.aruco.DetectorParameters()
+arucoDetector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+
+while True:
+    try:
+
+        tello.send_keepalive()
+        tello_image = tello.get_frame_read().frame
+
+        # Resize the image for display
+        #true_image = cv2.resize(tello_image, (640, 480))
+        true_image = cv2.cvtColor(tello_image, cv2.COLOR_BGR2RGB)
+
+        # Detect ArUco markers in the image
+        corners, ids, rejected = arucoDetector.detectMarkers(true_image)
+
+        # If ArUco markers are detected, print their IDs
+        if ids is not None:
+            draw_num(ids[0])
+            #tello.land()
+            break
+
+    except KeyboardInterrupt:
+        tello.streamoff()
+        tello.land()
+        print("Terminating...")
+        break
+
+tello.land()
